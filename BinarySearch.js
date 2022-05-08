1f38c50b04bbf619c7e9008f77907a9e885c2276
sajibsaha4647 <sajibsahabdtask@gmail.com>
2022-05-08 10:49:01 +0600

    updated

--- BinarySearch.js
+++ BinarySearch.js
@@ -11,26 +11,26 @@ BinarySearch = (arr, val) => {
   let results;
   arr.map((items, i) => {
     if (items === val) {
-      results = i;
+      return (results = i);
     }
   });
 
   return results;
 
   // let start = 0;
-  // let end = arrays.length - 1;
-  // // while (start <= end) {
-  // let middle = Math.round((end - start) / 2);
+  // let end = arr.length - 1;
+  // while (start <= end) {
+  //   let middle = Math.round((end - start) / 2);
 
-  // if (middle === val) return middle;
+  //   if (arr[middle] === val) return middle;
 
-  // if (val > arrays[middle]) {
-  //   start = middle + 1;
-  // } else if (val < arrays[middle]) {
-  //   end = middle - 1;
-  // }
+  //   if (val > arr[middle]) {
+  //     start = middle + 1;
+  //   } else if (val < arr[middle]) {
+  //     end = middle - 1;
+  //   }
   // }
   // return -1;
 };
 
-console.log(BinarySearch([2, 3, 34, 13, 4, 7, 35, 36], 2));
+console.log(BinarySearch([2, 3, 34, 13, 4, 7, 35, 36], 4));
