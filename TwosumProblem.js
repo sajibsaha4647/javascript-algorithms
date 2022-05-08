0a3d6c63cf2731a43d5972844bdb38a303909ba1
sajibsaha4647 <sajibsahabdtask@gmail.com>
2022-05-08 14:55:36 +0600

    updated

---
+++ TwosumProblem.js
@@ -0,0 +1,9 @@
+getTwosum = (nums, target) => {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) return [i, j];
+    }
+  }
+};
+
+console.log(getTwosum([2, 4, 5, 67, 3, 8], 9));
