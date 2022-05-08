e001a8ffe9f381f4453be12c262a941da83f87a8
sajibsaha4647 <sajibsahabdtask@gmail.com>
2022-05-08 11:39:46 +0600

    updated

---
+++ BubbleSort.js
@@ -0,0 +1,15 @@
+GetBubblesor = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] < arr[j + 1]) {
+        console.log(arr);
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(GetBubblesor([2, 35, 23, 1 - 45]));
