0fd375eca83f14c9c078e72af9ac3ba4b4c471f2
sajibsaha4647 <sajibsahabdtask@gmail.com>
2022-05-08 12:52:44 +0600

    add selection sort

--- SelectionSort.js
+++ SelectionSort.js
@@ -2,15 +2,18 @@ SelectionSort = (arr) => {
   let lowest;
   for (let i = 0; i < arr.length; i++) {
     lowest = i;
+
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[lowest] > arr[j]) {
         lowest = j;
       }
     }
-    let temp = arr[i];
-    arr[i] = arr[lowest];
-    arr[lowest] = temp;
+    if (i !== lowest) {
+      let temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
   }
   return arr;
 };
-console.log(SelectionSort([23, 45, -6, 45, 238]));
+console.log(SelectionSort([000, 1000, 3000, 00, 00]));
