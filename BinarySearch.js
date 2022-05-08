f3e361d7532cd175b1b6c89d9ec02338e8ed5774
sajibsaha4647 <sajibsahabdtask@gmail.com>
2022-05-08 10:18:46 +0600

    add binary search

--- BinarySearch.js
+++ BinarySearch.js
@@ -1,26 +1,36 @@
 BinarySearch = (arr, val) => {
-  let arrays = [];
-  let arryd = arr.sort((a, b) => {
-    //array sorting
-    if (a > b) return 1;
-    if (a < b) return -1;
-    return 0;
-  });
-  arrays.push(arryd);
-  let start = 0;
-  let end = arrays.length - 1;
-  while (start <= end) {
-    let middle = Math.round((end - start) / 2);
+  // let arrays = [];
+  // let arryd = arr.sort((a, b) => {
+  //   //array sorting
+  //   if (a > b) return 1;
+  //   if (a < b) return -1;
+  //   return 0;
+  // });
+  // arrays = arryd;
 
-    if (middle === val) return middle;
-
-    if (val > arrays[middle]) {
-      start = middle + 1;
-    } else if (val < arrays[middle]) {
-      end = middle - 1;
+  let results;
+  arr.map((items, i) => {
+    if (items === val) {
+      results = i;
     }
-  }
-  return -1;
+  });
+
+  return results;
+
+  // let start = 0;
+  // let end = arrays.length - 1;
+  // // while (start <= end) {
+  // let middle = Math.round((end - start) / 2);
+
+  // if (middle === val) return middle;
+
+  // if (val > arrays[middle]) {
+  //   start = middle + 1;
+  // } else if (val < arrays[middle]) {
+  //   end = middle - 1;
+  // }
+  // }
+  // return -1;
 };
 
-console.log(BinarySearch([2, 3, 34, 13, 4, 7, 35, 36], 34));
+console.log(BinarySearch([2, 3, 34, 13, 4, 7, 35, 36], 2));
