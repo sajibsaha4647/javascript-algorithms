f89036618215244c3fe033de5e928928812ccc17
sajibsaha4647 <sajibsahabdtask@gmail.com>
2022-05-08 11:46:01 +0600

    add bubble sor

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,4 +1,5 @@
-GetBubblesor = (arr) => {
+GetBubblesort = (arr) => {
+  let iswap = false;
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - 1; j++) {
       if (arr[j] < arr[j + 1]) {
@@ -6,10 +7,12 @@ GetBubblesor = (arr) => {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        iswap = true;
       }
     }
+    if (!iswap) break;
   }
   return arr;
 };
 
-console.log(GetBubblesor([2, 35, 23, 1 - 45]));
+console.log(GetBubblesort([2, 35, 23, 1 - 45]));
