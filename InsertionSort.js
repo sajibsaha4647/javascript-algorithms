2711e27c19bfc2ed756d17b0eb3bd4835bb5738e
sajibsaha4647 <sajibsahabdtask@gmail.com>
2022-05-08 14:10:50 +0600

    updated

---
+++ InsertionSort.js
@@ -0,0 +1,14 @@
+InsertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = i; j > 0; j--) {
+      if (arr[j] > arr[j - 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(InsertionSort([40, 56, 78, 78, 33, -45, 500]));
